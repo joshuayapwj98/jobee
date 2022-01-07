2410f694124ba5986ccd7227d246f0ad64e6de80
Joshua <e0544333@u.nus.edu>
2022-01-07 17:48:24 +0800

    Add Registration screen input fields
    - Added input fields for registration screen
    - Added data validation

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -16,4 +16,10 @@ export default {
     tabIconDefault: '#ccc',
     tabIconSelected: tintColorDark,
   },
+  input: {
+    text: '#000000',
+    primary: '#560CCE',
+    secondary: '#414757',
+    error: '#f13a59',
+  }
 };
--- screens/RegisterScreen.tsx
+++ screens/RegisterScreen.tsx
@@ -1,18 +1,74 @@
 import * as React from 'react';
+import { useState } from 'react';
 import { View, Text } from '../components/Themed';
 import { Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import { emailValidator } from '../helpers/emailValidator'
+import { passwordValidator } from '../helpers/passwordValidator'
+import { nameValidator } from '../helpers/nameValidator'
+
 import { Platform, StyleSheet } from 'react-native';
+import TextInput from '../components/TextInput'
 
 export default function RegisterScreen({ navigation }) {
+    const [name, setName] = useState({ value: '', error: '' })
+    const [email, setEmail] = useState({ value: '', error: '' })
+    const [password, setPassword] = useState({ value: '', error: '' })
+
+    const onSignUpPressed = () => {
+        const nameError = nameValidator(name.value)
+        const emailError = emailValidator(email.value)
+        const passwordError = passwordValidator(password.value)
+        if (emailError || passwordError || nameError) {
+            setName({ ...name, error: nameError })
+            setEmail({ ...email, error: emailError })
+            setPassword({ ...password, error: passwordError })
+            return
+        }
+        navigation.navigate('Root')
+    }
+
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Registration</Text>
             <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+
+            <TextInput
+                label="Name"
+                returnKeyType="next"
+                value={name.value}
+                onChangeText={(text) => setName({ value: text, error: '' })}
+                error={!!name.error}
+                errorText={name.error}
+            />
+
+            <TextInput
+                label="Email"
+                returnKeyType="next"
+                value={email.value}
+                onChangeText={(text) => setEmail({ value: text, error: '' })}
+                error={!!email.error}
+                errorText={email.error}
+                autoCapitalize="none"
+                autoCompleteType="email"
+                textContentType="emailAddress"
+                keyboardType="email-address"
+            />
+
+            <TextInput
+                label="Password"
+                returnKeyType="done"
+                value={password.value}
+                onChangeText={(text) => setPassword({ value: text, error: '' })}
+                error={!!password.error}
+                errorText={password.error}
+                secureTextEntry
+            />
+
             <Button title="Go to Details"
-                onPress={() => navigation.navigate('Root')} />
+                onPress={onSignUpPressed} />
         </View>
     );
 }
